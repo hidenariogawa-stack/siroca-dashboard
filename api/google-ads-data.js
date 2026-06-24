5ce92700eafa5ec86cbb0b07f4a103554e96fedf
hidenariogawa-stack <hidenari_ogawa@siroca.co.jp>
2026-06-24 17:45:13 +0900

    Update google-ads-data.js

--- api/google-ads-data.js
+++ api/google-ads-data.js
@@ -39,7 +39,7 @@ export default async function handler(req, res) {
     `;
 
     const adsRes = await fetch(
-      `https://googleads.googleapis.com/v17/customers/${customerId}/googleAds:search`,
+      `https://googleads.googleapis.com/v19/customers/${customerId}/googleAds:search`,
       {
         method: 'POST',
         headers: {
